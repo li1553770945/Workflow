bea0cdd44401baae7f192ab3e61c6cbb5aaa5ccd
li1553770945 <1553770945@qq.com>
2022-12-09 23:37:29 +0800

    feat(structure):完成基本结构搭建

---
+++ infra/conf/conf.go
@@ -0,0 +1,35 @@
+package conf
+
+import (
+	"gopkg.in/yaml.v3"
+	"os"
+)
+
+type HttpConfig struct {
+	ListenAddress string `yaml:"listen-address"`
+	Mode          string `yaml:"mode"`
+}
+type MysqlConfig struct {
+	Username string `yaml:"username"`
+	Password string `yaml:"password"`
+	Database string `yaml:"database"`
+	Address  string `yaml:"address"`
+}
+type Config struct {
+	HttpConfig  HttpConfig  `yaml:"http"`
+	MysqlConfig MysqlConfig `yaml:"mysql"`
+}
+
+func NewConfig(path string) *Config {
+	conf := &Config{}
+	f, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	err = yaml.NewDecoder(f).Decode(conf)
+	if err != nil {
+		panic(err)
+	}
+
+	return conf
+}
